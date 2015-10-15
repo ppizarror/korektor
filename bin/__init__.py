5d08f0767f4733c16dd52c776e05053160ce3877
Pablo Pizarro <pablo@ppizarror.com>
2015-10-15 14:33:58 -0300

    Agregados __init__.py

---
+++ bin/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Posee módulos utilitarios y programas externos
+
+# BIN
+# Autor: PABLO PIZARRO @ ppizarro
+# Fecha: OCTUBRE 2015
+# Licencia: GPLv2
---
+++ lib/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Maneja las funciones principales de korektor
+
+# LIB
+# Autor: PABLO PIZARRO @ ppizarro
+# Fecha: OCTUBRE 2015
+# Licencia: GPLv2
