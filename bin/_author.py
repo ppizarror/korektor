82400345b323fe79ac8cf2318163c20d4eef0ad4
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-04-05 13:49:59 -0400

    Actualiza correo

--- bin/_author.py
+++ bin/_author.py
@@ -12,5 +12,5 @@ Licencia: GPLv2
 __author__ = 'Pablo Pizarro [@ppizarror]'
 # noinspection SpellCheckingInspection
 __credits__ = ['Pablo pizarro [@ppizarror]', 'Marko Kreen', 'John J. Lee', '@ponty']
-__email__ = 'pablo.pizarro@ing.uchile.cl'
+__email__ = 'pablo@ppizarror.com'
 __maintainer__ = 'Pablo Pizarro [@ppizarror]'
